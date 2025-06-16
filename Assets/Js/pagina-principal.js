3fddb49cb1482b8596547dc91347ffbe7531a81b
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-06-16 13:02:54 -0300

    adicao scroll efect js

--- Assets/Js/pagina-principal.js
+++ Assets/Js/pagina-principal.js
@@ -12,4 +12,14 @@ fecharModal.addEventListener('click', () => {
   overlay.style.display = 'none'
 })
 
-// /* ------------------------------------------ */
+/* Scroll rolagem */
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('[data-header]')
+
+  if (window.scrollY > 10) {
+    header.classList.add('rolagem')
+  } else {
+    header.classList.remove('rolagem')
+  }
+})
